1561ffd659071ef43e3f4a34eaf5d9445aa5230d
ZZ <shadow1088punch@gmail.com>
2023-11-02 11:21:59 +0100

    code optimalization1.0

--- codes/pygame0.py
+++ codes/pygame0.py
@@ -15,6 +15,12 @@ class Settings:
     window_size = "Normal"
 sets = Settings()
 
+###xxx###
+
+# RESOLUTIONS
+
+###xxx###
+
 if sets.resolution not in sets.resolutions:
     print("Invalid resolution.")
 
@@ -41,50 +47,57 @@ elif sets.window_size == "Full":
 ###########################################################################
 ###########################################################################
 
-
+#GAME SETTINGS
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Meteor shooter - ZZ")
 
+#IMAGES
 ship = pygame.image.load("graphics/ship.png").convert_alpha()
 background = pygame.image.load("graphics/background.png").convert()
 
+#TEXT
 font_size = 50
 font = pygame.font.Font("graphics/subatomic.ttf", font_size)
 text0 = font.render("Meteor shooter", True, "grey50")
 
+#OTHER
 i = 1
 BREAK = False
-# sizes
 
-
-
+#sizes
 ###########################################################################
 ###########################################################################
 
 while True:
-    # events
+    #### events
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    # updates
-    screen.fill("grey13")
-    # location of surfaces
-    screen.blit(background, (0, 0))
     
+    #### updates 
+    
+    screen.fill("grey13")
+    
+    # surfaces(blit and location)
+    screen.blit(background, (0, 0))
     
     screen.blit(ship, (i, math.floor(screen_height/4*3.1)))
     
+    # MAIN SCREEN SHIP MOVEMENT
     if i >= screen.get_width() - ship.get_width():
         BREAK = True
+    elif i <= 0:
+        BREAK = False
         
     if BREAK == True:
         i = i - 0.27*x
     else: i = i + 0.27*x
+
+    #MAIN SCREEN TEXT
     screen.blit(text0, (screen_width/2 - text0.get_width()/2, screen_height/2-(30*y) - text0.get_height()/2))
-    
-    if i <= 0:
-        BREAK = False
+   
         
 
                
