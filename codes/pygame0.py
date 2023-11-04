110471f9b6c53b14a201aae0fd1695230d935c65
ZZ <shadow1088punch@gmail.com>
2023-11-04 20:46:02 +0100

    settings v1.0

--- codes/pygame0.py
+++ codes/pygame0.py
@@ -57,20 +57,23 @@ ship = pygame.image.load("graphics/ship.png").convert_alpha()
 background = pygame.image.load("graphics/background.png").convert()
 START_butt = pygame.image.load("graphics/START.png").convert_alpha()
 EXIT_butt = pygame.image.load("graphics/EXIT.png").convert_alpha()
-
+SETTINGS_butt = pygame.image.load("graphics/SETTINGS.png").convert_alpha()
+SETTINGS_butt_scale = pygame.transform.scale(SETTINGS_butt, (37*x,37*y))
 
 #TEXT
 font_size = 50
 font = pygame.font.Font("graphics/subatomic.ttf", font_size)
 text0 = font.render("Meteor shooter", True, "grey50")
+gameplay_text0 = font.render("Gameplay: ", True, "grey50")
 
 #OBJECT SETTINGS
 ship_rect = ship.get_rect(center = (screen_width/2, math.floor(screen_height/4*3.1)))
 start_rect = START_butt.get_rect(center = (screen_width/2 - 200*x, screen_height/2 + 30*y))
 exit_rect = EXIT_butt.get_rect(center = (screen_width/2 + 200*x, screen_height/2 + 30*y))
+settings_rect = SETTINGS_butt.get_rect(center = (screen_width - 50*x, 10*y))
 #OTHER
 i = 1
-BREAK = False
+MENU_BOUNCE = False
 clock = pygame.time.Clock()
 MENU = True
 SETTINGS = False
@@ -100,7 +103,7 @@ def shoot():
 ###########################################################################
 ###########################################################################
 
-print(start_rect.y); print(start_rect.height)
+
 
 while True:
     #### events
@@ -112,9 +115,14 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == pygame.BUTTON_LEFT:
             if start_rect.x <= mouse[0] <= start_rect.x + start_rect.width and start_rect.y <= mouse[1] <= start_rect.y + start_rect.height:
                 MENU = False
-            elif exit_rect.x <= mouse[0] <= exit_rect.x + exit_rect.width and exit_rect.y <= mouse[1] <= exit_rect.y + exit_rect.height:
+            if exit_rect.x <= mouse[0] <= exit_rect.x + exit_rect.width and exit_rect.y <= mouse[1] <= exit_rect.y + exit_rect.height:
                 pygame.quit()
                 sys.exit()
+            if settings_rect.x <= mouse[0] <= settings_rect.x + settings_rect.width and settings_rect.y <= mouse[1] <= settings_rect.y + settings_rect.height:
+                SETTINGS = True
+                MENU = False
+            
+
         
         if event.type == pygame.MOUSEMOTION and MENU == False and Settings.gameplay == "Mouse":
             ship_rect.center = event.pos
@@ -154,7 +162,7 @@ while True:
     if move_right == True:
         ship_rect.x += 10
 
-
+    
     #### 
     screen.fill("grey13")
     clock.tick(80)
@@ -164,17 +172,19 @@ while True:
 
     # surfaces(blit and location)
     screen.blit(background, (0, 0))
+    screen.blit(SETTINGS_butt_scale, (screen_width - SETTINGS_butt.get_width()+30*x, 20))
+    
     #/#screen.blit(ship, (i, ship_rect.y + ship.get_height()/2))
     
     # MAIN SCREEN SHIP MOVEMENT
     if MENU == True:
         screen.blit(ship, (i, ship_rect.y + ship.get_height()/2))
         if i >= screen.get_width() - ship.get_width():
-            BREAK = True
+            MENU_BOUNCE = True
         elif i <= 0:
-            BREAK = False
+            MENU_BOUNCE = False
             
-        if BREAK == True:
+        if MENU_BOUNCE == True:
             i = i - 3*x
         else: i = i + 3*x
 
@@ -183,9 +193,10 @@ while True:
         #MAIN SCREEN BUTTONS
         screen.blit(START_butt, start_rect)
         screen.blit(EXIT_butt, exit_rect)
-    else:
+    if MENU == False and SETTINGS == False:
         screen.blit(ship, ship_rect)
-    
+    if SETTINGS == True:
+        screen.blit(text0, (screen_width/2 - text0.get_width()/2, screen_height/2-(30*y) - text0.get_height()/2))
     ### SETTINGS
         
 
